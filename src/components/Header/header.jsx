a8591295a606e0f7542568db4e33a9449c2caa84
Lloydfaharon <143098047+Lloydfaharon@users.noreply.github.com>
2024-09-05 18:40:25 +0200

    ajout du logo

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,14 @@
 import "./header.css";
+import Image from"../../image/d2.jpg"
 
 
 function App() {
   return (
     <nav className="navigation">
-      <h1>Devarl.com</h1>
+      <div className="block-logo">
+        <img src={Image} className="image-header" alt="logo du site"/> 
+        <h1>evarl.com</h1>
+      </div>
       <div className="link">
         <a href="#about">A propos</a>
         <a href="#projects">Projets</a>
